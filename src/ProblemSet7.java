588b2a70d508b2e3965abe53d130111fc4d7f1fd
Aidan Forrester <agregforrester@gmail.com>
2020-10-25 16:54:12 -0400

    finished

--- src/ProblemSet7.java
+++ src/ProblemSet7.java
@@ -16,6 +16,8 @@ public class ProblemSet7 {
         System.out.println(aidan.triplets("aaabbbccc"));
         System.out.println(aidan.addMe("123 abc 123"));
         System.out.println(aidan.sequence("abbcccdddd"));
+        System.out.println(aidan.intertwine("aceg", "bdfh"));
+        System.out.println(aidan.isPalindrome("racecar"));
     }
 
     /*
@@ -211,7 +213,40 @@ public class ProblemSet7 {
      * Given a string, compute the length of the longest sequence.
      */
     public long sequence(String text) {
-        return 0;
+        char character;
+        char nextCharacter;
+        int counter = 1;
+        int number = 1;
+
+        if (text == null) {
+            return -1;
+        } else if (text.isEmpty() || text.isBlank()) {
+            return 0;
+        } else if (text.length() == 1) {
+            return 1;
+        } else {
+            for (int n = 0; n < text.length(); n++) {
+                character = text.charAt(n);
+
+                if (n + 1 == text.length()) {
+                    break;
+                } else {
+                    nextCharacter = text.charAt(n + 1);
+                }
+
+                if (character == nextCharacter) {
+                    counter++;
+                }
+
+                if (counter > number) {
+                    number = counter;
+                    counter = 1;
+                }
+            }
+
+            return number;
+        }
+
     }
 
     /*
@@ -221,7 +256,60 @@ public class ProblemSet7 {
      * characters of a and b.
      */
     public String intertwine(String a, String b) {
-        return "";
+        char aCharacter;
+        char bCharacter;
+        String intertwine = "";
+        int n = 0;
+
+        if (a == null || b == null) {
+            return null;
+        } else {
+
+
+            if (a.length() > b.length()) {
+                for (n = 0; n < a.length(); n++) {
+                    if (n != b.length()) {
+                        aCharacter = a.charAt(n);
+                        bCharacter = b.charAt(n);;
+
+                        intertwine = intertwine + aCharacter + bCharacter;
+                    } else {
+                        break;
+                    }
+                }
+
+                for (int j = n; j < a.length(); j++) {
+                    aCharacter = a.charAt(j);
+                    intertwine = intertwine + aCharacter;
+                }
+            } else if (b.length() > a.length()) {
+                for (n = 0; n < b.length(); n++) {
+                    if (n != a.length()) {
+                        aCharacter = a.charAt(n);
+                        bCharacter = b.charAt(n);;
+
+                        intertwine = intertwine + aCharacter + bCharacter;
+                    } else {
+                        break;
+                    }
+                }
+
+                for (int j = n; j < b.length(); j++) {
+                    bCharacter = b.charAt(j);
+                    intertwine = intertwine + bCharacter;
+                }
+            } else {
+                for (n = 0; n < b.length(); n++) {
+                    aCharacter = a.charAt(n);
+                    bCharacter = b.charAt(n);;
+
+                    intertwine = intertwine + aCharacter + bCharacter;
+                }
+            }
+            return intertwine;
+        }
+
+
     }
     /*
      * Exercise 10.
@@ -229,7 +317,31 @@ public class ProblemSet7 {
      * Given a string, determine whether or not it is a palindrome.
      */
     public boolean isPalindrome(String text) {
-        return true;
+        char character1;
+        char character2;
+        int n = 0;
+
+
+        if (text == null) {
+            return false;
+        } else {
+            int j = text.length() - 1;
+
+            while (n < Math.ceil(text.length() / 2) && j > Math.floor(text.length() / 2)) {
+                character1 = text.charAt(n);
+                character2 = text.charAt(j);
+
+                if (character1 != character2) {
+                    return false;
+                }
+
+                n++;
+                j--;
+            }
+
+            return true;
+        }
+
     }
 
 
